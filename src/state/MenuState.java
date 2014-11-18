4482b1c11597fb176adb0d1aa854ef5e7fba3fc7
Swinny <williamwswinny@gmail.com>
2014-11-18 17:22:30 -0600

    Added Class Base

---
+++ src/state/MenuState.java
@@ -0,0 +1,12 @@
+package state;
+
+/************************************************************************
+ * Class: MenuState														
+ * Author: William Swinny											
+ * 																		
+ * Purpose: The menu that will be shown when the user starts the game																	
+ ************************************************************************/
+public class MenuState
+{
+	
+}
---
+++ src/state/State.java
@@ -0,0 +1,13 @@
+package state;
+
+/************************************************************************
+ * Class: State														
+ * Author: William Swinny											
+ * 																		
+ * Purpose: An abstract class that will define the requirements for a
+ * 			game state																	
+ ************************************************************************/
+public class State
+{
+	
+}
