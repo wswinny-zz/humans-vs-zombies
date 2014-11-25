202e78bb5aa0a8c1aac8827b1014fe4039c2bb2b
Tara Reeves <tarafreeves@gmail.com>
2014-11-24 21:16:28 -0600

    Wrote in functionality for sock class. Also, began work on objective.

--- src/game/GamePanel.java
+++ src/game/GamePanel.java
@@ -20,7 +20,7 @@ public class GamePanel extends JPanel{
 	private boolean running;
 
 	public static final int WIDTH = 320;
-	public static final int HEIGHT = GamePanel.WIDTH * 3 / 4;
+	public static final int HEIGHT = (GamePanel.WIDTH * 3 / 4);
 	public static final int SCALE = 2;
 	public static final int FPS = 60;
 	
