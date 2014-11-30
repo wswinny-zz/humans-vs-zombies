12bbff036168eeda5821d41c2b7fd4618319f1af
Tara Reeves <tarafreeves@gmail.com>
2014-11-30 17:33:07 -0600

    An objective will now generate in a new, random location when the player reaches it. There is also now a set duration for the objective, and when that time is up, functionality is in place in the update function for it to reset to a new location. Zombie/Player multiplier added. Now just need to figure out how to have it go away after 10 seconds.

--- src/entity/Objective.java
+++ src/entity/Objective.java
@@ -5,12 +5,14 @@ import game.GamePanel;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.Date;
 import java.util.Random;
 
 import javax.imageio.ImageIO;
 
 import map.Map;
 import map.Tile;
+import state.ObjectiveMode;
 import util.Audio;
 
 /************************************************************************
@@ -21,27 +23,13 @@ import util.Audio;
  ************************************************************************/
 public class Objective extends Entity{
 	private static int objectiveDuration;
+	private long startTime;
 	private boolean playerReached = false;
 	
 	//Constructor
 	public Objective(){
-		//Generate position 200 from the left and right
-		Random random = new Random();
-		double xPos = random.nextInt((Map.getVisibleMap()[0].length*Map.getTileSize()));
-		double yPos = random.nextInt((Map.getVisibleMap()[0].length*Map.getTileSize()));
-
-		//if it intersected try again
-		while(intersectsWithMap(xPos, yPos)){
-			xPos = random.nextInt((Map.getVisibleMap()[0].length*Map.getTileSize()));
-			yPos = random.nextInt((Map.getVisibleMap()[0].length*Map.getTileSize()));
-		}		
-		
-		xPos = Player.getInstance().getX() + 100;
-		yPos = Player.getInstance().getY() + 100;
-		
-		//Set the x and y position of the objective
-		this.setX(xPos);
-		this.setY(yPos);
+		//Generate objective and its random position/duration
+		generateNewObjective();
 		
 		//Set the image of the objective if it hasn't been set already
 		if(this.getImg() == null){
@@ -55,18 +43,54 @@ public class Objective extends Entity{
 		}
 	}
 	
+	private void generateNewDuration(int distance){
+		//Generate a new duration
+		objectiveDuration = distance * 3;
+		
+		//Store start time so objective can keep track of 
+		//time limit
+		
+	}
+	
+	//Generates a random, new location for the objective
+	private void generateNewObjective(){
+		//Generate random position
+		Random random = new Random();
+		double xPos = random.nextInt((Map.getVisibleMap()[0].length*Map.getTileSize()));
+		double yPos = random.nextInt((Map.getVisibleMap()[0].length*Map.getTileSize()));
+
+		//if it intersected with a non-reachable block, try again
+		while(intersectsWithMap(xPos, yPos)){
+			xPos = random.nextInt((Map.getVisibleMap()[0].length*Map.getTileSize()));
+			yPos = random.nextInt((Map.getVisibleMap()[0].length*Map.getTileSize()));
+		}	
+				
+		//Set the x and y position of the objective
+		this.setX(xPos);
+		this.setY(yPos);
+		
+		//Find the distance between the player and the new objective
+		int distance = (int)Math.sqrt((xPos-this.getX())*(xPos-this.getX()) 
+				+ (yPos-this.getY())*(yPos-this.getY()));
+		
+		//Generate a new duration time accordingly
+		generateNewDuration(distance);
+	}
+	
 	//Determines whether or not the position given intersects with the map
 	public boolean intersectsWithMap(double xPos,double yPos){
 		Tile[][] tiles = Map.getVisibleMap();
 		int tileWidth = tiles[0][0].getImage().getWidth();
+		
 		
 		int xTile = (int)(xPos/tileWidth);
 		int yTile = (int)(yPos/tileWidth);
 		
 		if(xTile < 0 || yTile < 0) return true;
 		if(xTile >= tiles[0].length || yTile >= tiles.length) return true;
-		if(tiles[yTile][xTile].getTileType() == Tile.BLOCKED) return true;
-	
+		if(tiles[yTile][xTile].getTileType() == Tile.BLOCKED 
+				|| tiles[yTile][xTile].getTileType() == Tile.ZOMBIE_BLOCKED) return true;
+		
 		return false;
 	}
 
@@ -86,15 +110,49 @@ public class Objective extends Entity{
 			return false;
 	}
 	
+	//Updates the objective. If it has already been reached, then create a new
+	//location
+	public void update(){
+		//If the player has reached the objective,
+		//generate a new objective in a different location
+		if(playerReached){
+			generateNewObjective();
+			this.playerReached = false;
+		}
+		
+		//Obtain the value of the time elapsed
+		long timeElapsed = (new Date().getTime() - this.startTime)/1000;
+		
+		//If the duration for the objective is up, apply a speed boost properly
+		//and generate a new objective in a different location
+		if(timeElapsed >= objectiveDuration){
+			if(objectiveReached()){
+				//Give the player a speed boost
+				Player.getInstance().setSpeedMultiplier(
+						Player.getInstance().getSpeedMultiplier() + 0.5);
+			}
+			else{
+				Audio.getInstance().playObjFailed();
+				
+				//Give the zombies a speed boost
+				ObjectiveMode.increaseZombieSpeedMultiplier();
+			}
+			
+			//Generate the new objective
+			generateNewObjective();
+			this.playerReached = false;
+		}
+	}
+	
 	//Draw the objective onto the panel
 	@Override
 	public void draw(Graphics g){
 		double playerX = Player.getInstance().getX();
 		double playerY = Player.getInstance().getY();
 		
-		if(!playerReached){
+		if(!this.playerReached){
 			if(objectiveReached())
-				playerReached = true;
+				this.playerReached = true;
 		}
 
 		//Paint the objective
@@ -113,5 +171,13 @@ public class Objective extends Entity{
 
 	public static void setObjectiveDuration(int objectiveDuration) {
 		Objective.objectiveDuration = objectiveDuration;
+	}
+
+	public long getStartTime() {
+		return startTime;
+	}
+
+	public void setStartTime(long startTime) {
+		this.startTime = new Date().getTime();
 	}
 }
