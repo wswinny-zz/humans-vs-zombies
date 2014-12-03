c9e1b339026905248750214a5fd7a519ebbfb313
Tara Reeves <tarafreeves@gmail.com>
2014-12-02 19:38:10 -0600

    Dividing by speed multiplier now rather than multiplying by it to get the objectiveDuration.

--- src/entity/Objective.java
+++ src/entity/Objective.java
@@ -45,7 +45,7 @@ public class Objective extends Entity{
 	
 	private void generateNewDuration(int distance){
 		//Generate a new duration
-		objectiveDuration = (double)(distance / 50) * Player.getInstance().getSpeedMultiplier();
+		objectiveDuration = (double)(distance / 50) / Player.getInstance().getSpeedMultiplier();
 		
 		//Store start time so objective can keep track of 
 		//time limit
